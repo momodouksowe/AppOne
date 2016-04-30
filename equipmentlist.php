d1a2b2320f242e8e18c1bdbfee8de4c37a4146f5
momodouksowe <momodouksowe@gmail.com>
2016-05-01 00:57:28 +0100

    Equipmentlist file
    File Admin should see when successfully login

---
+++ equipmentlist.php
@@ -0,0 +1,39 @@
+<?php
+	session_start();
+	if(!isset($_SESSION['userId'])){
+		header("Location: loginAjax.php");
+		exit();
+	}
+	$row = $_SESSION['userId'];
+
+?>
+<html>
+	<head>
+	<link rel="stylesheet" href="../css/style.css">
+		<title>Equipment</title>
+
+		<div id = "headerBar">
+		<div id = "logo">
+		<img src="../images/logo.png"></img>
+		</div>
+
+		<div id = "menu">
+		<ul>
+		<li><a href="logout.php">logout</a></li>
+		</ul>
+		</div>
+		</div>
+
+    </head>
+    <body>
+    <center>
+    <h1> Equipment List Page!</h1>
+    </center>
+    </body>
+</html>	
+
+		
+	
+
+	
+
